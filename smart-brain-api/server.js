6f2d771afc5644cb2c0412c7c89f57773c3d82c9
dsouzalenroy <54295102+dsouzalenroy@users.noreply.github.com>
2020-09-20 18:13:48 +1000

    Refined and made changes by removing repitative lines of code

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -4,6 +4,12 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin');
+const image = require('./controllers/image');
+const profile = require('./controllers/profile');
+
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -25,123 +31,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// const database = {
-//   users: [
-//     {
-//       id: '123',
-//       name: 'John',
-//       email: 'john@gmail.com',
-//       password: 'cookies',
-//       entries: 0,
-//       joined: new Date()
-//     },
-//     {
-//       id: '124',
-//       name: 'Sally',
-//       email: 'sally@gmail.com',
-//       password: 'bananas',
-//       entries: 0,
-//       joined: new Date()
-//     },
-//   ],
-//
-// }
 
-app.get('/', (req, res) =>{
-  res.send(database.users);//database variable no longer exists
-})
 
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash); // true
-      if(isValid){
-        return db.select('*').from('users')
-          .where('email', '=', req.body.email)
-          .then(user =>{
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'))
-      }else{
-        res.status(400).json('wrong credentials')
-      }
-    })
-    .catch(err => res.status(400).json('wrong credentials'))
-})
-
-app.post('/register',(req, res) => {
-  const { email, name, password} = req.body;
-  // bcrypt.hash(password, null, null, function(err, hash) {
-  // });
-
-  //start of hashing passwords using bcrypt synchronous
-  const saltRounds = 10;
-  const salt = bcrypt.genSaltSync(saltRounds);
-  const hash = bcrypt.hashSync(password, salt);
-  //end of hashing passwords using bcrypt
-  db.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail => {
-      return trx('users')
-      .returning('*')
-      .insert({
-        email: loginEmail[0],
-        name: name,
-        joined: new Date()
-      })
-      .then(user => {
-          res.json(user[0]);
-        })
-    })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })
-    .catch(err => res.status(400).json('unable to register'))
-})
-
-app.get('/profile/:id', (req,res) =>{
-  const {id} = req.params;
-  db.select('*').from('users').where({ id })
-    .then(user => {
-      if(user.length){
-        res.json(user[0])
-      }else{
-        res.status(400).json('Not found')
-      }
-    })
-    .catch(err => res.status(400).json('error getting user'))
-})
-
-app.put('/image', (req, res) =>{
-  const {id} = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries',1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get an entry'))
-})
+app.get('/', (req, res) =>{res.send(database.users) })//database variable no longer exists
+// app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+// the above line can also be written as below
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register',(req, res) => {register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) =>{image.handleImage(req, res, db)})
 
 app.listen(3000, () =>{
   console.log('app is running on port  3000');
 })
-
-
-
-// // Load hash from your password DB.
-
-
-/*
-/--> res = this is working
-/signin --> POST --> success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT --> user
-*/
--- src/App.js
+++ src/App.js
@@ -27,23 +27,26 @@ const particlesOptions ={
     }
   }
 }
+
+const initialState = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+      id: '',
+      name: '',
+      email: '',
+      entries: 0,
+      joined: ''
+    }
+}
+
 class App extends Component{
   constructor(){
     super ()
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) =>{
@@ -97,6 +100,8 @@ displayFaceBox = (box) => {
         .then(count =>{
           this.setState(Object.assign(this.state.user, { entries: count }))
         })
+        .catch(console.log)
+
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
@@ -105,7 +110,7 @@ displayFaceBox = (box) => {
 
 onRouteChange = (route) => {
   if (route === 'signout'){
-    this.setState({isSignedIn: false})
+    this.setState(initialState)
   }else if(route === 'home'){
     this.setState({isSignedIn: true})
   }
